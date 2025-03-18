98ee7cc6ba2543da6e821a2644e119d8b28c5bba
Sergey Gorshkov <gorshkov.s@extyl-pro.ru>
2025-03-18 11:38:10 +0300

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about',[PageController::class, 'about'])->name('about');
